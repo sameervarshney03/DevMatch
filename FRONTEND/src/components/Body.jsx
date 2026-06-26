b371f3bff1576b38627a28a289ce039358e2ad26
Sameer Varshney <samv12761@gmail.com>
2026-06-26 19:08:04 +0530

    Keep user logged in on page refresh

--- FRONTEND/src/components/Body.jsx
+++ FRONTEND/src/components/Body.jsx
@@ -1,8 +1,35 @@
-import { NavBar } from "./NavBar";
-import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { BASE_URL } from "../utils/constant";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try {
+      if (user) return; // If already logged in, do nothing
+      
+      const res = await axios.get(BASE_URL + "/profile", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      // If error (e.g., token expired or no token), maybe navigate to login or just do nothing
+      if (window.location.pathname === "/") {
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col justify-between">
         <Outlet></Outlet>
